099b3a50f3c37bcb47d58b508767a385d2e042f4
YU-CHEN CHIU <119109468+bensonchiuhe@users.noreply.github.com>
2026-07-19 13:25:01 +0800

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newStr = ''
+
+        for c in s:
+            if c.isalnum():
+                newStr += c.lower()
+        if newStr != newStr[::-1]:
+                return False
+        return True
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
